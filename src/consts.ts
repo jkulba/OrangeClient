c07ad66bdeab5ac06e13c2e27321915262f8aac3
Jim Kulba <jkulba@gmail.com>
2025-11-10 20:58:22 -0800

    Sanity

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Orange Client";
+export const SITE_DESCRIPTION = "Welcome to the Orange Client!";
